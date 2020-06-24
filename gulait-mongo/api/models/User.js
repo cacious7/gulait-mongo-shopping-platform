55966b050fdc7250aac8a92419b0b8467fad530b
Cacious Siamunyanga <csiamunyanga@hotmail.com>
2020-06-24 15:58:25 +0200

    edited the User model

--- gulait-mongo/api/models/User.js
+++ gulait-mongo/api/models/User.js
@@ -3,12 +3,57 @@ const mongoose = require( 'mongoose' );
 const UserSchema = new mongoose.Schema( {
     userName: {
         type: String,
-        required: true
+        required: true,
+        unique: true
+    },
+    firstName: {
+        type: String,
+        required: true,
+    },
+    lastName: {
+        type: String,
+        required: true,
+    },
+    phone: String,
+    email: {
+        type: String,
+        required: true,
+        unique: true
     },
     password: {
         type: String,
         required: true
-    }
+    },
+    avatarUrl: {
+        type: String,
+    },
+    dateOfBirth: {
+        type: Date
+    },
+    wishlists: [
+        {
+            name: String,
+            products: [
+                {
+                    productId: ObjectId,
+                    variationId: ObjectId
+                }
+            ]
+        }
+    ],
+    cart: [
+        {
+
+            products: [
+                {
+                    productId: ObjectId,
+                    variationId: ObjectId
+                }
+            ]
+        }
+    ]
+
+
 } );
 
 module.exports = mongoose.model( 'User', UserSchema );
