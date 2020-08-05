392c5899a429bc752c9fa5c5c0709d9c363e3891
cacious siamunyanga <csiamunyanga@hotmail.com>
2020-08-05 09:59:00 +0200

    Start implementing the category search feature
    A user should be able to retrieve the list of categories
    This is to allow the categories to be displayed on the front end
    interface

--- gulait-mongo/api/routes/store.js
+++ gulait-mongo/api/routes/store.js
@@ -2,15 +2,38 @@ const router = require( 'express' ).Router();
 const authenticateAccessToken = require( '../../util/auth/authenticateAccessToken' );
 const grantSellerAccessToStore = require( '../../util/auth/grantSellerAccessToStore' );
 const joi = require( 'joi' );
-const Store = require( '../models/Store' );
+const Store = require( '../models/Store' );\
+const generateCategoryQuery = require( '../../util/auth/generateCategoryQuery' );
 
 /**
  * Get a list of stores
  */
-router.get();
+router.get( async () => {
+    conts validationSchema = joi.object().keys( {
+        category: joi.string().allow( '' )
+    } );
+
+    joi.validate( req.body, validationSchema, ( err, results ) => {
+        if( err ) res.status( 401 ).json( {
+            message: 'Error', data: err  
+        } );
+    } );
+
+    if( !headersSent ){
+        const query = generateCategoryQuery( req.body.category );
+        if( query ){
+            const categories = Store.find( query == true );
+            if( categories ){
+                return
+            }  
+        }
+        const store = await Store.find( {} );
+    }
+} );
 
 /**
  * update a store
+ * need authorization
  */
 router.patch( '/update', authenticateAccessToken, grantSellerAccessToStore, async () => {
     try {
