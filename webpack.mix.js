6d54e23d12e94a48f7fa34623549b482cf7007aa
Cacious Siamunyanga <csiamunyanga@hotmail.com>
2020-09-26 13:26:54 +0200

    Deploy app to heroku

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,27 +11,11 @@ let mix = require( 'laravel-mix' );
  |
  */
 
-
-
 mix.react('resources/js/index.jsx', 'views/js/index.js') //this file gets loaded for mobiles
    .sass('resources/sass/style.scss', 'views/css/style.css') //this converts scss to css for use in our react app
-//    .options({
-//         hmrOptions: { //hmr stands for hot module replacement, alternatively called hot reloading or auto reloading
-//             host: 'localhost',
-//             port: 3000
-//         },
-//   })
-
-    //Hot reloading with browser sync
-    //requires the following dependencies
-    //browser-sync and browser-sync-webpack-plugin
-    //.browserSync( 'localhost' )
 
     //PREVENT fs module not found problems
     .webpackConfig({node: {fs: 'empty'}});
 
-//   if (mix.inProduction()) {
-//     mix.version();
-//   }
    
    
